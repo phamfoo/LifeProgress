b6a0c3a69f152432e0a7ac1ef5b8a570249b0f03
Tien Pham <tien@phaw.io>
2022-05-25 21:15:54 +0700

    Draw life calendar from hardcoded DOB

--- LifeProgress/ContentView.swift
+++ LifeProgress/ContentView.swift
@@ -3,37 +3,67 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            Canvas { context, size in
+            if let (age, weekOfYear) = getAgeComponents() {
                 let totalWeeksInAYear = 52
                 let averageLifeExpectancy = 72
-                let cellSize = size.width / CGFloat(totalWeeksInAYear)
-                let cellPadding = cellSize / 8
+                let progress =
+                    (Double(age) + Double(weekOfYear) / Double(totalWeeksInAYear)) /
+                    Double(averageLifeExpectancy) * 100
+                let formattedProgress = String(format: "%.1f", progress)
 
-                for rowIndex in 0 ..< averageLifeExpectancy {
-                    for colIndex in 0 ..< totalWeeksInAYear {
-                        let cellPath =
-                            Path(CGRect(
-                                x: CGFloat(colIndex) * cellSize + cellPadding,
-                                y: CGFloat(rowIndex) * cellSize + cellPadding,
-                                width: cellSize - cellPadding * 2,
-                                height: cellSize - cellPadding * 2
-                            ))
+                Canvas { context, size in
+                    let cellSize = size.width / CGFloat(totalWeeksInAYear)
+                    let cellPadding = cellSize / 8
 
-                        if rowIndex <= 29 {
-                            context.fill(cellPath, with: .color(.gray))
-                        } else {
-                            context.stroke(
-                                cellPath,
-                                with: .color(.gray),
-                                lineWidth: cellSize / 8
-                            )
+                    for rowIndex in 0 ..< averageLifeExpectancy {
+                        for colIndex in 0 ..< totalWeeksInAYear {
+                            let cellPath =
+                                Path(CGRect(
+                                    x: CGFloat(colIndex) * cellSize + cellPadding,
+                                    y: CGFloat(rowIndex) * cellSize + cellPadding,
+                                    width: cellSize - cellPadding * 2,
+                                    height: cellSize - cellPadding * 2
+                                ))
+
+                            if rowIndex < age || rowIndex == age && colIndex <=
+                                weekOfYear
+                            {
+                                context.fill(cellPath, with: .color(.gray))
+                            } else {
+                                context.stroke(
+                                    cellPath,
+                                    with: .color(.gray),
+                                    lineWidth: cellSize / 8
+                                )
+                            }
                         }
                     }
                 }
+                .navigationTitle("Life Progress: \(formattedProgress)%")
+                .padding()
             }
-            .padding()
-            .navigationTitle("Life Progress: \(40)%")
         }
+    }
+
+    func getAgeComponents() -> (age: Int, weekOfYear: Int)? {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withFullDate]
+
+        if let dob = formatter.date(from: "1969/6/9") {
+            let ageComponents = Calendar.current.dateComponents(
+                [.year, .weekOfYear],
+                from: dob,
+                to: Date.now
+            )
+
+            if let age = ageComponents.year,
+               let weekOfYear = ageComponents.weekOfYear
+            {
+                return (age, weekOfYear)
+            }
+        }
+
+        return nil
     }
 }
 
