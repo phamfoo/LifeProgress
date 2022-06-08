406787e84eba7479f803c48b2956896b54202576
Quan Tran <quan@shousio.com>
2022-06-08 00:43:20 -0700

    Update the onboarding screen

--- LifeProgress/Views/WelcomeScreen.swift
+++ LifeProgress/Views/WelcomeScreen.swift
@@ -4,30 +4,51 @@ struct WelcomeScreen: View {
     @State private var showingProfile = false
 
     var body: some View {
-        VStack {
-            Image(systemName: "calendar")
-                .foregroundColor(.accentColor)
-                .font(.system(size: 100))
+        VStack(alignment: .leading) {
+            Spacer()
+            
+            Image("icon")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .cornerRadius(12.0)
 
-            Text("Welcome!")
-                .font(.title)
-                .padding([.top])
-
-            Text("Before we continue, let's set up your profile")
-                .foregroundColor(.secondary)
-                .multilineTextAlignment(.center)
-
-            Button("I'm ready") {
-                showingProfile = true
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Welcome to")
+                    .font(.system(size: 34))
+                
+                Text("Life Progress")
+                    .bold()
+                    .font(.system(size: 34, weight: .heavy))
             }
-            .padding([.top])
-            .buttonStyle(.bordered)
+            .padding(.bottom, 4)
+            
+            Text("Friendly reminder that you're not gonna live forever.")
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.leading)
+            
+            Spacer()
+            
+            VStack {
+                Text("Before we continue, let's set up your profile")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.center)
+                
+                Button {
+                    showingProfile = true
+                } label: {
+                    Text("Continue")
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.borderedProminent)
+                .controlSize(.large)
+            }
         }
         .padding()
         .sheet(isPresented: $showingProfile) {
-            ProfileScreen(onDone: {
-                showingProfile = false
-            })
+            ProfileScreen()
+                .interactiveDismissDisabled()
         }
     }
 }
