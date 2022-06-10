3d1421348b0dd37b8cd4c8a7104dc5500fcdf4d6
Tien Pham <tien@phaw.io>
2022-06-10 15:28:05 +0700

    Increase default life expectancy

--- LifeProgress/Models/Profile.swift
+++ LifeProgress/Models/Profile.swift
@@ -5,7 +5,7 @@ let extensionDefaults = UserDefaults(suiteName: "group.io.phaw.LifeProgress")!
 
 extension Defaults.Keys {
     static let lifeExpectancy = Key<Int>("lifeExpectancy",
-                                         default: 72,
+                                         default: 79,
                                          suite: extensionDefaults)
     static let birthday = Key<Date>("birthday", default: Life.getDefaultBirthday(), suite: extensionDefaults)
     static let profileSetupCompleted = Key<Bool>("profileSetupCompleted", default: false, suite: extensionDefaults)
