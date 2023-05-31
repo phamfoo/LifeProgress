99c5e79c09a974210743cb1b476e3a77622d86a1
Tien Pham <tien@pham.foo>
2023-05-31 16:06:25 +0700

    Update app icon

--- LifeProgress/Views/OnboardingScreen.swift
+++ LifeProgress/Views/OnboardingScreen.swift
@@ -6,34 +6,31 @@ struct OnboardingScreen: View {
     var body: some View {
         VStack(alignment: .leading) {
             Spacer()
-            
-            Image("icon")
-                .resizable()
-                .frame(width: 100, height: 100)
-                .cornerRadius(12)
+
+            Image("RoundedIcon")
 
             VStack(alignment: .leading, spacing: 0) {
                 Text("Welcome to")
                     .font(.system(size: 34))
-                
+
                 Text("Life Progress")
                     .bold()
                     .font(.system(size: 34, weight: .heavy))
             }
             .padding(.bottom, 4)
-            
+
             Text("Friendly reminder that you're not gonna live forever.")
                 .foregroundColor(.secondary)
                 .multilineTextAlignment(.leading)
-            
+
             Spacer()
-            
+
             VStack {
                 Text("Before we continue, let's set up your profile")
                     .font(.subheadline)
                     .foregroundColor(.secondary)
                     .multilineTextAlignment(.center)
-                
+
                 Button {
                     showingProfile = true
                 } label: {
